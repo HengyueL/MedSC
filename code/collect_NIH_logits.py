ea760c181f8a96505eb22dea2716cc9d9f7e4dce
Hengyue Liang <liang656@ahl01.agate.msi.umn.edu>
2024-02-08 13:53:24 -0600

    Add

--- code/collect_NIH_logits.py
+++ code/collect_NIH_logits.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--ckpt_dir", dest="ckpt_dir", type=str,
-        default="/panfs/jay/groups/15/jusun/shared/For_HY/SC_eval/models/NIH/best.pt"
+        default="/panfs/jay/groups/15/jusun/shared/For_HY/SC_eval/models/NIH/decoupling-cRT_nih.pt"
     )
     args = parser.parse_args()
     main(args)
