6fafa3521c73829dfb3511d7a59fbc2f709574b5
liang hengyue <lianghengyue1993@outlook.com>
2024-02-14 12:39:23 -0600

    Add

--- code/plot_rc_curve_in_d.py
+++ code/plot_rc_curve_in_d.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--exp_dir", dest="exp_dir", type=str,
-        default="NIH-res50",
+        default="HAM\\wce",
         help="Experiment subfolder where collected data are located."
     )
     args = parser.parse_args()
